5e54b95e8dc39179d2173db1ed81ad3e2849eb0e
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2023-11-25 15:02:29 +0530

    Added an npm package to print one-liner jokes and also tried figlet package as well

--- NPM/index.js
+++ NPM/index.js
@@ -10,11 +10,29 @@
 //     console.log(`My name is ${sillyname}.`);
 // }
 
-import superheroes from "superheroes";
+// import superheroes from "superheroes";
 
 
 
-for (var i =0;i<5;i++){
-    const name = superheroes.random();
-    console.log(`i am ${name}! `);
-}
+// for (var i =0;i<5;i++){
+//     const name = superheroes.random();
+//     console.log(`i am ${name}! `);
+// }
+
+
+// let oneLinerJoke = require('one-liner-joke');
+// console.log(oneLinerJoke.getRandomJoke());
+
+
+/// FIGLET CODE
+
+var figlet = require("figlet");
+
+figlet("Hello World!!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
