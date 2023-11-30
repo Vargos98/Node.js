df8a641fc4e5783a4048bae07d530be3779d1513
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2023-11-30 10:16:20 +0530

    Started revising Node.js, Express.js, MongoDB and Mongoose

--- index.js
+++ index.js
@@ -3,14 +3,24 @@
 // let c = a+b
 // console.log(c);
 
-const fs = require("fs");
+// const fs = require("fs");
 
 // fs.writeFile("message.txt","hello from NodeJs!", (err)=>{
 //     if(err) throw err;
 //     console.log("the file has been saved!"); 
 // });
 
-fs.readFile('./message.txt','utf-8',(err,data)=>{
-    if(err) throw err;
-    console.log(data);
-});
+// fs.readFile('./message.txt','utf-8',(err,data)=>{
+//     if(err) throw err;
+//     console.log(data);
+// });
+
+
+const money = true;
+
+if(money){
+    console.log("Buy whole bunch of stuff");
+}
+else{
+    console.log("You iz broke !");
+}
